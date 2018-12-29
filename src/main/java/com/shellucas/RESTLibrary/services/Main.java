04a4c62afffd78e38b7c6f858bfc0f07c26371d4
Shelby <shelby.hendrickx@hotmail.com>
2018-12-29 15:24:34 +0100

    Initial Commit

---
+++ src/main/java/com/shellucas/RESTLibrary/services/Main.java
@@ -0,0 +1,13 @@
+package com.shellucas.RESTLibrary.services;
+
+public class Main {
+
+    public Main() {
+    }
+
+    public int testMethod() {
+        return 24;
+    }
+
+
+}
---
+++ src/test/java/com/shellucas/RESTLibrary/services/MainTest.java
@@ -0,0 +1,23 @@
+package com.shellucas.RESTLibrary.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void testMethod() {
+    }
+
+    
+
+}
